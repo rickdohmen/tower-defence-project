69507df97bf5184744d11b8c1423d8d29cfa036b
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-09-21 14:04:15 +0200

    hopelijk de kleine update in de tower code

--- Assets/scripts/turret.cs
+++ Assets/scripts/turret.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class turret : MonoBehaviour
 {
+    public Transform target;
     List<GameObject> Enemy = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,7 @@ public class turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Vector3.zero);
     }
 
     private void OnTriggerEnter(Collider other)
